b3a92d7416684ca8edcc142ce915d17665141bea
jk <kerntzj@gmail.com>
2019-04-12 10:30:25 -0700

    add toppings to pizza in console

--- js/scripts.js
+++ js/scripts.js
@@ -7,11 +7,20 @@ function Pizza(size, crust, toppings) {
   this.size = size;
   this.crust = crust;
   this.toppings = [];
+  this.addToppings(toppings);
+}
+
+Pizza.prototype.addToppings = function(toppings) {
+  for (var i = 0; i <= toppings.length -1; i++){
+    this.toppings.push(toppings[i]);
+  }
 }
 
 PizzaOrder.prototype.addPizza = function (pizza) {
   this.pizzas.push(pizza);
+  this.pizzaId ++;
 };
 
+
 var newOrder = new PizzaOrder();
-var newPizza = new Pizza("Lg", "Pan", "pepperoni", "mushroom", "beef")
+var newPizza = new Pizza("Lg", "Pan", ["pepperoni", "mushroom"])
