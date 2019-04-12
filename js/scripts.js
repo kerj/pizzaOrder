d0afbafd3f0c79d70de4c2c9797a9436fc37135d
jk <kerntzj@gmail.com>
2019-04-12 13:22:12 -0700

    add for loops and finalized backend functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,59 +1,60 @@
 function PizzaOrder() {
   this.pizzas = [];
-  this.pizzaId = 0;
   this.orderTotal = 0;
+}
+
+PizzaOrder.prototype.finalizeOrder = function() {
+  for (var i = 0; i <= this.pizzas.length -1; i++) {
+    this.orderTotal += this.pizzas[i].pricePer;
+  }
+  return this.orderTotal
 }
 
 function Pizza(size, crust, toppings) {
   this.size = size;
   this.crust = crust;
   this.toppings = [];
+  this.pricePer = 0;
+  this.pizzaId = 0;
   this.addToppings(toppings);
 }
 
-PizzaOrder.prototype.pricedPizza = function(newPizza) {
-  var pizzaPrice = 0;
-  var orderTotal = 0;
-
-  this.pizzas.forEach(function() {
-    if (newPizza.crust != "Hand-tossed"){
-      pizzaPrice += 1;
-    }
-    if (newPizza.size === "Small"){
-      pizzaPrice += 8;
-    }else if (newPizza.size === "Medium") {
-      pizzaPrice += 12;
-    }else{pizzaPrice += 14}
-    if (newPizza.toppings.length >= 2) {
-        pizzaPrice -= 1.5;
-      newPizza.toppings.forEach(function() {
-        pizzaPrice += 1.5;
-    })
-  }
-
-    // while(newPizza.toppings.length >= 2){
-    //   pizzaPrice += 1.5;
-    //   newPizza.toppings.pop();
-    // }
-    orderTotal += pizzaPrice;
-  })
-  this.orderTotal = orderTotal;
-}
-
 Pizza.prototype.addToppings = function(toppings) {
+  this.pizzaId += newOrder.pizzas.length
   for (var i = 0; i <= toppings.length -1; i++){
     this.toppings.push(toppings[i]);
   }
+  this.addPizza();
 }
 
-PizzaOrder.prototype.addPizza = function (pizza) {
-  this.pizzas.push(pizza);
-  this.pizzaId ++;
+Pizza.prototype.pricedPizza = function() {
+  for (var i=0; i <= this.pizzaId; i++){
+    this.pricePer = 0;
+    var pizzasPrice = this.pricePer
+    if (this.crust != "Hand-tossed"){
+      pizzasPrice += 1;
+    }
+    if (this.size === "Small"){
+      pizzasPrice += 8;
+    }else if (this.size === "Medium") {
+      pizzasPrice += 12;
+    }else{pizzasPrice += 14}
+    if (this.toppings.length >= 2) {
+      pizzasPrice -= 1.5;
+      this.toppings.forEach(function() {
+        pizzasPrice += 1.5;
+      })
+    }
+    this.pricePer += pizzasPrice;
+  }
+}
+
+Pizza.prototype.addPizza = function () {
+  newOrder.pizzas.push(this);
+  this.pricedPizza()
 };
 
-
 var newOrder = new PizzaOrder();
-var newPizza = new Pizza("Large", "Pan", ["pepperoni", "mushroom", "Bell pepper", "Extra-Cheese"])
-console.log(newOrder.addPizza(newPizza));
-console.log(newOrder.pricedPizza(newPizza));
-console.log(newOrder);
+var newPizza = new Pizza("Large", "Pan", ["pepperoni", "mushroom", "Bell pepper", "Extra-Cheese", "pepperoni", "Extra-Cheese", "Mushroom", "Pineapple"])
+var newPizza2 = new Pizza("Large", "Pan", ["pepperoni", "Extra-Cheese"])
+var newPizza3 = new Pizza("Medium", "Thin", ["Mushroom", "Pineapple"])
